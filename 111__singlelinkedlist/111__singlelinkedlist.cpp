bcf18e7227cfa869f9c66c7dcd840c649b57b017
syihwamoza <mozakastella26@gmail.com>
2024-04-26 12:14:13 +0700

    untuk pemrosesan pilihan menu dalam program utama

--- 111__singlelinkedlist/111__singlelinkedlist.cpp
+++ 111__singlelinkedlist/111__singlelinkedlist.cpp
@@ -131,6 +131,47 @@ void traverse() {
 				cout << "5. Keluar" << endl;
 				cout << "Pilihan";
 				cin >> pilihan;
+				switch (pilihan)
+				{
+				case 1:
+					addNode()
+						cout << "Data Berhasil Ditambahkan" << endl;
+					system("pause");
+					system("cls");
+					break;
+				case 2:
+					if (listEmpty())
+					{
+						cout << "List kosong" << endl;
+						system("pause");
+						system("cls");
+						break;
+					}
+
+					int nim;
+					cout << "Masukkan NIM";
+					cin >> nim;
+					if (deleteNode(nim)) {
+						cout << "nim:" << nim << "berhasil dihapus" << endl;
+						system("pause");
+						system("cls");
+					}
+					else
+						cout << "Data tidak ditemukan" << endl;
+					break;
+				case 3:
+					traverse();
+					break;
+				case 4:
+					searchData();
+					break;
+				case 5:
+					break;
+				default:
+					cout << "Piliha tidak ada" << endl;
+					break;
+				}
+			
 				
 
 
