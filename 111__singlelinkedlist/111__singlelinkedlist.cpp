a923ff23cd4fb0fd3b1f9332ba567ac1d9235006
syihwamoza <mozakastella26@gmail.com>
2024-04-26 11:19:05 +0700

    memasukkan fungsi untuk menambahkan node baru ke linked list

--- 111__singlelinkedlist/111__singlelinkedlist.cpp
+++ 111__singlelinkedlist/111__singlelinkedlist.cpp
@@ -20,5 +20,19 @@ void addNode() {
 	nodeBaru->noMhs = nim;
 	nodeBaru->name = nama;
 
+	if (START == NULL || nim <= = START->noMhs) {
+		if (START != NULL && nim == START->noMhs)
+		{
+			cout << "NIM sudah ada" << endl;
+			return;
+		}
+
+		nodeBaru->next = START;
+		START = nodeBaru;
+		return;
+	}
+
+	Node* previous = START;
+	Node* current = START;
 
 }
