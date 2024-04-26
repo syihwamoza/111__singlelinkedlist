e842ea29d1a1f2909bd241445a3dc98dfa7a9de0
syihwamoza <mozakastella26@gmail.com>
2024-04-26 12:03:35 +0700

    untuk menu program utama

--- 111__singlelinkedlist/111__singlelinkedlist.cpp
+++ 111__singlelinkedlist/111__singlelinkedlist.cpp
@@ -118,6 +118,19 @@ void traverse() {
 		}
 	}
 	int main (){
-	
+		int pilihan;
+		do
+		{
+			try
+			{
+
+				cout << "1. Tambah Data" << endl;
+				cout << "2. Hapus Data" << endl;
+				cout << "3. Tampilkan Data" << endl;
+				cout << "4. Cari Data" << endl;
+				cout << "5. Keluar" << endl;
+				cout << "Pilihan";
+				cin >> pilihan;
+				
 
 
